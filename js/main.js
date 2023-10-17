cc67c6348b0f90faf0502a1f7816fca3d9aea6b3
rmcobol1 <lstrmcobol123@naver.com>
2023-10-17 13:35:31 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,230 @@
+const searchEl=document.querySelector('.search');
+const searchInputEl=searchEl.querySelector('input');
+
+searchEl.addEventListener('click',function(){
+    //Logic..
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus',function(){
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder','통합검색');
+});
+//focus가 해제되면
+searchInputEl.addEventListener('blur',function(){
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder','');
+});
+
+
+const badgeEl=document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
+
+
+/*
+    구글 검색:lodash cdn
+    cdnjs.com -> 설명
+    lodash.js  -> 클릭
+*/
+
+window.addEventListener('scroll',_.throttle(function(){
+    // console.log('scroll!');
+    console.log(window.scrollY);
+    if (window.scrollY > 500){
+        //배지 숨기기
+        // badgeEl.style.display='none';
+        // gsap.to(요소, 지속시간, 옵션)
+        gsap.to(badgeEl, .6, {
+            opacity:0,
+            display:'none'
+        });
+        // 버튼 보이기!
+        gsap.to(toTopEl,.2,{
+          x:0
+        });
+    }else{
+        //배지 보이기
+        // badgeEl.style.display='block';
+        gsap.to(badgeEl, .6, {
+            opacity:1,
+            display:'block'
+        });
+
+        // 버튼 숨기기!
+        gsap.to(toTopEl,.2,{
+          x:100
+        });
+    }
+},300));
+
+//_.throttle(함수, 시간)
+
+
+// const toTopEl = document.querySelector('#to-top');
+toTopEl.addEventListener('click',function(){
+  gsap.to(window, .7,{
+    // GSAP & ScrollToPlugin
+    scrollTo:0
+  });
+});
+
+
+
+/*
+    애니메이션
+    구글:gsap cdn
+    cdnjs.com
+    gsap-: 클릭
+*/
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEls,index){
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(fadeEls,1,{
+        delay:(index + 1) * .7, //0.7초, 1.4초, 2.1초, 2.7초
+        opacity:1
+    });
+});
+
+
+
+//공지사항
+//swiper
+/* 
+ Swiper
+ https://swiperjs.com
+ 버전에 따라 사용 방법이 다름(강의는 6버전)
+ 
+ 구글:swiperjs 
+ swiperjs.com
+
+*/
+//new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container',{
+    direction:'vertical',
+    autoplay:true,
+    loop:true
+});
+
+
+// 풀모션 클릭시
+new Swiper('.promotion .swiper-container', {
+    // direction: 'horizontal', // 수평 슬라이드
+    autoplay: { // 자동 재생 여부
+      delay: 5000 // 5초마다 슬라이드 바뀜
+    },
+    loop: true, // 반복 재생 여부
+    slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    pagination: { // 페이지 번호 사용 여부
+      el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+      clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+      prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+      nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
+    }
+  });
+  
+  
+
+  
+  // AWARDS
+
+  new Swiper('.awards .swiper-container', {
+    // direction: 'horizontal', // 수평 슬라이드
+    autoplay: true, // 자동 재생 여부
+    loop: true, // 반복 재생 여부
+    spaceBetween: 30, // 슬라이드 사이 여백
+    slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+    // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+      prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
+      nextEl: '.awards .swiper-next' // 다음 버튼 선택자
+    }
+  });
+
+
+
+  /**
+ * Promotion 슬라이드 토글 기능
+ */
+// 슬라이드 영역 요소 검색!
+const promotionEl = document.querySelector('.promotion')
+// 슬라이드 영역를 토글하는 버튼 검색!
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+// 슬라이드 영역 숨김 여부 기본값!
+let isHidePromotion = false
+// 토글 버튼을 클릭하면,
+promotionToggleBtn.addEventListener('click', function () {
+  // 슬라이드 영역 숨김 여부를 반댓값으로 할당!
+  isHidePromotion = !isHidePromotion
+  // 요소를 숨겨야 하면,
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide')
+  // 요소가 보여야 하면,
+  } else {
+    promotionEl.classList.remove('hide')
+  }
+})
+
+
+/*
+ * 부유하는 요소 관리
+ */
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 '문자 데이터'를,
+  // `parseFloat()`을 통해 소수점을 가지는 '숫자 데이터'로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+// 부유하는(떠 다니는) 요소를 만드는 함수
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소,시간,옵션);
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+      delay: random(0, delay), // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정.
+      y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
+      repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
+      yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
+      //구글:gsap easing 검색
+      //greensock.com
+      ease: Power1.easeInOut // Easing 함수 적용.
+    }
+  )
+}
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', .5, 15)
+floatingObject('.floating3', 1.5, 20)
+
+
+
+
+// scrollMagic cdn
+/**
+ * 요소가 화면에 보여짐 여부에 따른 요소 관리
+ */
+// 관리할 요소들 검색!
+const spyEls = document.querySelectorAll('section.scroll-spy')
+// 요소들 반복 처리!
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
+
+
+// footer 영역 날짜 정보
+const thisYear=document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();//2023
+
+
+
